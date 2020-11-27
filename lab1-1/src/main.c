12671f790876ea91e254c9c23c7ba6136c77d3f0
Maxim Muratov <m.muratov@g.nsu.ru>
2020-11-27 21:26:34 +0700

    Update main.c

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    return EXIT_SUCCESS;
+int main() {
+    FILE * input = fopen("in.txt", "r"), * output = fopen("out.txt", "w");
+    char symbol;
+    while(fscanf(input, "%c", &symbol) && !feof(input));
+    fprintf(output, "%d\n", symbol);
+    fclose(input);
+    fclose(output);
+    return 0;
 }
